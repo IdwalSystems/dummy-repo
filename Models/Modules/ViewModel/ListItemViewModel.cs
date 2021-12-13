b70cc33dc086c6c31d346ec3d84797d70e64b2fd
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2021-12-13 16:18:42 +0800

    Clean up UI for AkTerima Index, Detail, Edit, Create

---
+++ Models/Modules/ViewModel/ListItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSNK.Models.Modules.ViewModel
+{
+    public class ListItemViewModel
+    {
+        [NotMapped]
+        public int id { get; set; }
+        [NotMapped]
+        public string perihal { get; set; }
+    }
+}
