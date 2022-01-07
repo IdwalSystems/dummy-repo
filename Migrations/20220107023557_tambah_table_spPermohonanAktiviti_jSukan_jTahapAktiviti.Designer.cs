45261febf1a0727f18fce97b716a495ba80e3f5e
syafik000 <93978272+syafik000@users.noreply.github.com>
2022-01-07 10:38:13 +0800

    migration untuk spPermohonanAktiviti, jSukan, jTahapAktiviti

--- Migrations/20220107023557_tambah_table_spPermohonanAktiviti_jSukan_jTahapAktiviti.Designer.cs
+++ Migrations/20220107023557_tambah_table_spPermohonanAktiviti_jSukan_jTahapAktiviti.Designer.cs
@@ -4,14 +4,16 @@ using MSNK.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MSNK.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220107023557_tambah_table_spPermohonanAktiviti_jSukan_jTahapAktiviti")]
+    partial class tambah_table_spPermohonanAktiviti_jSukan_jTahapAktiviti
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
