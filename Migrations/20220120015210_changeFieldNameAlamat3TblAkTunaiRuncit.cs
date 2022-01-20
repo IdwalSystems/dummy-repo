8e5b43688e9445075c1648088bbe121994db7385
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2022-01-20 09:53:14 +0800

    rename field Alamat3 tbl AkTunaiCV

---
+++ Migrations/20220120015210_changeFieldNameAlamat3TblAkTunaiRuncit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MSNK.Migrations
+{
+    public partial class changeFieldNameAlamat3TblAkTunaiRuncit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Almat3",
+                table: "AkTunaiCV",
+                newName: "Alamat3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Alamat3",
+                table: "AkTunaiCV",
+                newName: "Almat3");
+        }
+    }
+}
--- Models/Modules/AkTunaiCV.cs
+++ Models/Modules/AkTunaiCV.cs
@@ -10,20 +10,25 @@ namespace MSNK.Models.Modules
     public class AkTunaiCV
     {
         public int Id { get; set; }
+        [DisplayName("Kod Kaunter")]
         public int AkTunaiRuncitId { get; set; }
         public AkTunaiRuncit AkTunaiRuncit { get; set; }
         public int KategoriPenerima { get; set; }
         public string Tahun { get; set; }
+        [DisplayName("No CV")]
         public string NoCV { get; set; }
         public DateTime Tarikh { get; set; }
+        [DisplayName("Kod Pekerja")]
         public int? SuPekerjaId { get; set; }
         public SuPekerja SuPekerja { get; set; }
+        [DisplayName("Kod Pembekal")]
         public int? AkPembekalId { get; set; }
         public AkPembekal AkPembekal { get; set; }
         public string Penerima { get; set; }
+        [DisplayName("Alamat")]
         public string Alamat1 { get; set; }
         public string Alamat2 { get; set; }
-        public string Almat3 { get; set; }
+        public string Alamat3 { get; set; }
         public string Catatan { get; set; }
         public decimal Jumlah { get; set; }
         public ICollection<AkTunaiCV1> AkTunaiCV1 { get; set; }
