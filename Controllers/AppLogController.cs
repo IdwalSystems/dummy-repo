df3a4299d478492863b089a722ae3786fb200b49
Aminudin Kahar <amin@idwal.com.my>
2022-10-13 15:29:14 +0800

    fix issue 0142, 0143 (KIV)

--- Controllers/AppLogController.cs
+++ Controllers/AppLogController.cs
@@ -8,7 +8,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MSNK.Data;
+using MSNK.Infrastructure;
 using MSNK.Models.Modules;
+using MSNK.Models.Modules.PrintModel;
+using Rotativa.AspNetCore;
 
 namespace MSNK.Controllers
 {
@@ -17,12 +20,15 @@ namespace MSNK.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserService _userService;
 
         public AppLogController(ApplicationDbContext context,
-            UserManager<IdentityUser> userManager)
+            UserManager<IdentityUser> userManager,
+            UserService userService)
         {
             _context = context;
             _userManager = userManager;
+            _userService = userService;
         }
 
         // GET: AppLog
@@ -95,6 +101,91 @@ namespace MSNK.Controllers
             // function search modul range end
             return View(appLog.OrderBy(b => b.LgDate).ThenBy(b => b.LgModule).ToList());
         }
-   
+
+        public async Task<IActionResult> PrintPdf(
+            string searchUsername,
+            string searchModulFrom,
+            string searchModulTo,
+            string searchDateFrom,
+            string searchDateTo)
+        {
+            var appLog = new List<AppLog>();
+
+            if (!String.IsNullOrEmpty(searchUsername) ||
+                !String.IsNullOrEmpty(searchModulFrom) ||
+                !String.IsNullOrEmpty(searchModulTo) ||
+                !String.IsNullOrEmpty(searchDateFrom) ||
+                !String.IsNullOrEmpty(searchDateTo))
+            {
+                appLog = _context.AppLog.ToList();
+            }
+
+            //function search userList
+            var userList = await _context.applicationUsers.Include(x => x.SuPekerja).ToListAsync();
+
+            List<SelectListItem> userSelect = new();
+            userSelect.Add(new SelectListItem() { Text = "-- Pilih Pengguna --", Value = "" });
+            foreach (var q in userList)
+            {
+                userSelect.Add(new SelectListItem() { Text = q.Nama + " (" + q.UserName + ")", Value = q.UserName });
+            }
+
+            if (!String.IsNullOrEmpty(searchUsername))
+            {
+                ViewBag.username = new SelectList(userSelect, "Value", "Text", searchUsername);
+                appLog = appLog.Where(x => x.UserId == searchUsername).ToList();
+            }
+            else
+            {
+                ViewBag.username = new SelectList(userSelect, "Value", "Text", "");
+            }
+            //function search userList end
+
+            //function search date
+            if (!String.IsNullOrEmpty(searchDateFrom) && !String.IsNullOrEmpty(searchDateTo))
+            {
+                DateTime date1 = DateTime.Parse(searchDateFrom);
+                DateTime date2 = DateTime.Parse(searchDateTo);
+
+                appLog = appLog.Where(x => x.LgDate >= date1 && x.LgDate <= date2).ToList();
+
+                ViewData["DateFrom"] = searchDateFrom;
+                ViewData["DateTo"] = searchDateTo;
+                //akAkaun = akAkaun.OrderByDescending(c => c.Tarikh.Date).ThenBy(c => c.Tarikh.TimeOfDay);
+
+            }
+            //function search date end
+
+            //function search modul range
+            if (!String.IsNullOrEmpty(searchModulFrom) && !String.IsNullOrEmpty(searchModulTo))
+            {
+                Tuple<string, string> range = Tuple.Create(searchModulFrom, searchModulTo);
+                appLog = appLog.Where(s =>
+                        range.Item1.CompareTo(s.LgModule.Substring(0, range.Item1.Length)) <= 0 &&
+                        s.LgModule.Substring(0, range.Item2.Length).CompareTo(range.Item2) <= 0)
+                        .ToList();
+
+                ViewData["ModulFrom"] = searchModulFrom;
+                ViewData["ModulTo"] = searchModulTo;
+            }
+            // function search modul range end
+
+            AppLogPrintModel data = new AppLogPrintModel();
+
+            data.CompanyDetail = await _userService.GetCompanyDetails();
+            data.AppLog = appLog;
+            data.AppUser = userList;
+
+            return new ViewAsPdf("AppLogPrintPdf", data)
+            {
+                PageMargins = { Left = 15, Bottom = 15, Right = 15, Top = 15 },
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait,
+                //CustomSwitches = "--footer-center \"  Tarikh: " +
+                //    DateTime.Now.Date.ToString("dd/MM/yyyy") + "            Mukasurat: [page]/[toPage]\"" +
+                //    " --footer-line --footer-font-size \"10\" --footer-spacing 1 --footer-font-name \"Segoe UI\"",
+                PageSize = Rotativa.AspNetCore.Options.Size.A4,
+            };
+        }
+
     }
 }
---
+++ Models/Modules/PrintModel/AppLogPrintModel.cs
@@ -0,0 +1,12 @@
+﻿using MSNK.Models.Administration;
+using System.Collections.Generic;
+
+namespace MSNK.Models.Modules.PrintModel
+{
+    public class AppLogPrintModel
+    {
+        public List<ApplicationUser> AppUser { get; set; }
+        public List<AppLog> AppLog { get; set; }
+        public CompanyDetails CompanyDetail { get; set; }
+    }
+}
