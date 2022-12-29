693dad4a2202a56126c1b6a18c6fb61ec03773ba
Aminudin Kahar <amin@idwal.com.my>
2022-12-29 17:53:07 +0800

    add feature penyata alir tunai

---
+++ Models/Modules/ViewModel/AbAlirTunaiViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace MSNK.Models.Modules.ViewModel
+{
+    public class AbAlirTunaiViewModel
+    {
+        public string NoAkaun { get; set; }
+        public string NamaAkaun { get; set; }
+        public string Bulan { get; set; }
+        public decimal Jan { get; set; }
+        public decimal Feb { get; set; }
+        public decimal Mac { get; set; }
+        public decimal Apr { get; set; }
+        public decimal Mei { get; set; }
+        public decimal Jun { get; set; }
+        public decimal Jul { get; set; }
+        public decimal Ogo { get; set; }
+        public decimal Sep { get; set; }
+        public decimal Okt { get; set; }
+        public decimal Nov { get; set; }
+        public decimal Dis { get; set; }
+        public decimal Jan2 { get; set; }
+        public decimal JumAkaun { get; set; }
+        // nota:
+        // KeluarMasuk: 0 = Baki Awal
+        // KeluarMasuk: 1 = Masuk
+        // KeluarMasuk: 2 = Keluar
+        // KeluarMasuk: 3 = Baki Akhir
+        public int KeluarMasuk { get; set; }
+    }
+}
