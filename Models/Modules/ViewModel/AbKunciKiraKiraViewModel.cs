1c53c7b296a1d8c8cfb8b5c02653811f839ae229
Aminudin Kahar <amin@idwal.com.my>
2023-01-01 22:46:02 +0800

    add feature kunci kira-kira with reports

---
+++ Models/Modules/ViewModel/AbKunciKiraKiraViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MSNK.Models.Modules.ViewModel
+{
+    public class AbKunciKiraKiraViewModel
+    {
+        public int Order { get; set; }
+        public string Jenis { get; set; }
+        public string NoAkaun { get; set; }
+        public string NamaAkaun { get; set; }
+        public decimal Amaun { get; set; }
+
+    }
+}
