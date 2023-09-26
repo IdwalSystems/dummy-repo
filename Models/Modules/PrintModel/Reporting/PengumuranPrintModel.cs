9ca51f1bd5701f053d97ae59b2bb0e6098a08e69
Amin Addin <amin@idwal.com.my>
2023-09-26 11:58:19 +0800

    change submenu to fix according to account flow

---
+++ Models/Modules/PrintModel/Reporting/PengumuranPrintModel.cs
@@ -0,0 +1,13 @@
+﻿using MSNK.Models.Operations;
+
+namespace MSNK.Models.Modules.PrintModel.Reporting
+{
+    public class PengumuranPrintModel
+    {
+        public string Kod { get; set; }
+        public string Nama { get; set; }
+        public decimal JumlahTunggakan { get; set; }
+        public decimal Tertunggak { get; set; }
+        public KelasTunggakan KelasTunggakan { get; set; }
+    }
+}
---
+++ Models/Operations/KelasTunggakan.cs
@@ -0,0 +1,12 @@
+﻿namespace MSNK.Models.Operations
+{
+    public enum KelasTunggakan
+    {
+        Hari30 = 0,
+        Hari60 = 1,
+        Hari90 = 2,
+        Hari180 = 3,
+        Hari365 = 4,
+        LebihHari365 = 5,
+    }
+}
